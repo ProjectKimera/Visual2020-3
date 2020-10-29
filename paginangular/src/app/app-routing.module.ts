37a3036e3952875c475ce53eb7de04d3d37217f3
dacperezce <dacperezce@unal.edu.co>
2020-10-29 18:07:01 -0500

    pagina angular v0.2

--- paginangular/src/app/app-routing.module.ts
+++ paginangular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { RepoComponent } from './repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/repo', pathMatch: 'full'},
+  {path: 'about', component: AboutComponent},
+  {path: 'repo', component: RepoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
